504742d9b80084d7d0da16d9354136432835da1c
tejas711 <tezzs967@gmail.com>
2023-05-07 16:53:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [issues, setIssues] = useState([]);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  useEffect(() => {
+    async function fetchIssues() {
+      const response = await fetch(`https://api.github.com/repositories/1296269/issues?page=${pageNumber}&per_page=5`);
+      const data = await response.json();
+      setIssues(data.map((issue) => issue.title));
+    }
+    fetchIssues();
+  }, [pageNumber]);
+
+  function handleNextClick() {
+    setPageNumber((prevPageNumber) => prevPageNumber + 1);
+  }
+
+  function handlePrevClick() {
+    if (pageNumber > 1) {
+      setPageNumber((prevPageNumber) => prevPageNumber - 1);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Page number {pageNumber}</h2>
+      <ol>
+        {issues.map((issue, index) => (
+          <li key={index}>{issue}</li>
+        ))}
+      </ol>
+      <button id="load_prev" onClick={handlePrevClick}>
+        Previous
+      </button>
+      <button id="load_next" onClick={handleNextClick}>
+        Next
+      </button>
     </div>
   );
 }
